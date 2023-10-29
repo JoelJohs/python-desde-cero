287c84b95eb7e8be4fe6e5849567e8a9e549ee9e
Joel Johs <jojohersa21@gmail.com>
2023-10-29 14:49:47 -0600

    Funciones aprendidas

---
+++ tipos basicos/numeros funciones.py
@@ -0,0 +1,17 @@
+import math  # Importa el modulo math
+
+print(round(3.1415926535, 2))  # Redondea a 2 decimales dando 3.14
+
+print(abs(-10))  # Valor absoluto de -10 dando 10
+
+print(math.ceil(4.2))  # Redondea al entero mayor dando 5
+
+print(math.floor(4.99999999))  # Redondea al entero menor dando 4
+
+print(math.isnan(10))  # Devuelve True si el valor es NaN (Not a Number)
+
+print(math.pow(2, 3))  # Devuelve 2 elevado a 3 dando 8
+
+print(math.sqrt(16))  # Devuelve la raiz cuadrada de 16 dando 4
+
+# link de la documentacion oficial de math de python https://docs.python.org/3/library/math.html
