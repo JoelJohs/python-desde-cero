a0882676351141cc778467f49219594ff2a0f428
Joel Johs <jojohersa21@gmail.com>
2023-10-29 13:28:21 -0600

    secuencia de escapes learned

---
+++ tipos basicos/secuencia escape.py
@@ -0,0 +1,24 @@
+curso = 'Ultimate "Python"'  # imprime comillas dobles dentro de comillas simples
+
+print(curso)
+
+# Ejemplo usando \ para escapar caracteres
+
+curso = "Ultimate \"Python\""  # imprime comillas dobles dentro de comillas dobles
+print(curso)
+
+# Ejemplos de secuencias de escape en python
+# \' muestra comilla simple
+# \" muestra comilla doble
+# \\ muestra barra invertida
+
+# Ejemplo utilizando todas las secuencias de escape
+curso = "Ultimate \"Python\" \'Curso desde\' \\0\\"
+print(curso)
+
+# otros ejemplos de uso de \
+# \n salto de linea
+# \t tabulacion
+
+curso = "Ultimate \"Python\" \n\tCurso desde 0"
+print(curso)
