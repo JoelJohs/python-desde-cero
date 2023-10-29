b86f476a53630d25d7af92fe688d4f2aac8d0422
Joel Johs <jojohersa21@gmail.com>
2023-10-29 13:40:54 -0600

    .

---
+++ tipos basicos/numeros.py
@@ -0,0 +1,35 @@
+numero = 2  # Entero
+decimal = 1.8  # flotante
+imaginario = 2 + 2j  # 2 + 2i (parte real + parte imaginaria)
+
+# Ejemplos basicos de operaciones matematicas
+
+print(1 + 1)  # suma
+print(2 - 1)  # resta
+print(2 * 2)  # multiplicacion
+print(7 / 2)  # division
+print(7 // 2)  # division entera sin decimales
+print(7 % 2)  # modulo (residuo de la division)
+print(3 ** 3)  # potencia
+print(9 ** 0.5)  # raiz cuadrada
+print(35 ** (1 / 3))  # raiz cubica
+print(82 ** (1 / 2))  # raiz cuadrada
+
+
+numero = numero + 2  # suma en asignacion
+print("numero", numero)
+
+# Otra forma de hacerlo
+numero = 2
+numero += 4  # suma en asignacion
+print("numero", numero)
+
+# Se puede hacer lo mismo con el resto de operaciones
+numero -= 2  # resta en asignacion
+print("numero", numero)
+
+numero *= 4  # multiplicacion en asignacion
+print("numero", numero)
+
+numero /= 3  # division en asignacion
+print("numero", numero)
