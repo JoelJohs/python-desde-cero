70e8c155a19f37527e20c4b7067da5ef3a6f3953
Joel Josafat Hernandez Saucedo <jojohersa21@gmail.com>
2023-10-29 19:31:13 -0600

    else-if learned

---
+++ control-flujo/02-if.py
@@ -0,0 +1,12 @@
+edad = 25
+
+# Condicional else-if (elif)
+if edad > 50: # si la condición es verdadera
+    print("puede ver la película con descuento") # se ejecuta este código
+elif edad > 17: # elif es una abreviación de else if
+    print("puedes ver la película")
+else: # si la condición es falsa
+    print("No puede ver la película") # se ejecuta este código
+# Fin del condicional
+
+print("fin del programa")
---
+++ control-flujo/03-if-ternario.py
@@ -0,0 +1,8 @@
+edad = 25
+
+if edad > 18:
+    mensaje = "Es mayor de edad" 
+else:
+    mensaje = "Es menor de edad"
+
+print(mensaje)
