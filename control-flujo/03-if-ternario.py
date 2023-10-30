a3c6d93a7f5891b6ea2f8fbd789f107bfbdd8c25
Joel Josafat Hernandez Saucedo <jojohersa21@gmail.com>
2023-10-29 19:36:57 -0600

    if ternario learned

--- control-flujo/03-if-ternario.py
+++ control-flujo/03-if-ternario.py
@@ -1,8 +1,12 @@
 edad = 25
 
-if edad > 18:
-    mensaje = "Es mayor de edad" 
-else:
-    mensaje = "Es menor de edad"
+mensaje = "Es mayor de edad" if edad > 18 else "Es menor de edad" # if ternario
 
 print(mensaje)
+
+# if edad > 18:
+#     mensaje = "Es mayor de edad" 
+# else:
+#     mensaje = "Es menor de edad"
+
+# print(mensaje)
