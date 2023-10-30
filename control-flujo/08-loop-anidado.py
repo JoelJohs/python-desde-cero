bb6f417790a4a3e6b22607e6a8f54bafa5e50979
Joel Josafat Hernandez Saucedo <jojohersa21@gmail.com>
2023-10-29 20:41:41 -0600

    Loop anidado learned

---
+++ control-flujo/08-loop-anidado.py
@@ -0,0 +1,4 @@
+for i in range(3):
+    for j in range(2):
+        print(f"[{i}, {j}]") # Imprime el valor de i y j en cada iteración anidada
+# En el ambito real se debe evitar el uso de bucles anidados, ya que puede consumir muchos recursos
