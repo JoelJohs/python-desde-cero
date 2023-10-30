58b3957cdeeaa0569dda6dd31faa68d7f5bfb202
Joel Josafat Hernandez Saucedo <jojohersa21@gmail.com>
2023-10-29 19:19:01 -0600

    control-flujo folder added

---
+++ control-flujo/01-comparadores.py
@@ -0,0 +1,7 @@
+print(1 > 2) # Imprime false porque 1 no es mayor que 2
+print(1 < 2) # Imprime true porque 1 es menor que 2
+print(1 >= 2) # Imprime false porque 1 no es mayor o igual que 2
+print(1 <= 2) # Imprime true porque 1 es menor o igual que 2
+print(1 == 2) # Imprime false porque 1 no es igual que 2
+print(1 != 2) # Imprime true porque 1 no es distinto que 2
+print(2 == "2") # Imprime false porque 2 no es igual que "2" porque este último es un string
